51ab9d9343f7e98abef582618a3b7c56fb666f44
DekuCool24 <dayanna.qs@gmail.com>
2020-11-18 14:30:17 -0600

    Create Consulta1Page.js

---
+++ src/Client/Consulta1Page.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import { Image } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import { Redirect }                              from 'react-router-dom';
+import './WelcomeClient.css';
+import logo from '../images/logo.png';
+
+class Consulta1Page extends Component {
+
+  constructor(props){
+    super(props)
+    this.state ={ 
+      generalLanguage:[
+        {name: '',
+        count: ''}
+      ],
+      items: [],
+      error: null,
+      isLoaded: false,
+      loginFlag: false
+    }
+  }
+
+  clickPresionado = (event) =>{
+    this.setState({
+      loginFlag: true
+  })
+  }
+
+  renderRedirect = () => {
+    if (this.state.loginFlag) {
+      return <Redirect to='/IntroPage' />
+    }
+  }
+
+  render () {
+    return (
+      <div className="Consulta1Page">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                
+                <Navbar.Brand href="./WelcomeClient">Bienvenido</Navbar.Brand>
+                
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                
+                <Navbar.Collapse id="responsive-navbar-nav">
+
+                    <Nav className="mr-auto">
+                    <Nav.Link href="./Consulta1Page">Modificar mis datos</Nav.Link>
+                    <Nav.Link href="./Consulta2Page">Búsqueda idiomas que Enseño</Nav.Link>
+                    <Nav.Link href="./Consulta3Page">Búsqueda idiomas que Enseño y que me enseñen</Nav.Link>
+                    <Nav.Link href="./Consulta4Page">Búsqueda idiomas que Enseño y que me enseñen por País</Nav.Link>
+                    <Nav.Link href="./Consulta5Page">Búsqueda idiomas que Enseño y que me enseñen por País y rango Edad</Nav.Link>
+                    <Nav.Link onClick={this.clickPresionado}>LogOut</Nav.Link>
+                    </Nav>
+
+                </Navbar.Collapse>
+                </Navbar>
+                {this.renderRedirect()};
+          <Container>
+            <Row>
+              <Col>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <Image src={logo} fluid />
+              <br/>
+              <br/>
+              <br/>
+                  <h1>Consulta #1</h1>
+                  <p>
+                  Esta es la página de la Consulta #1
+                  </p>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              <br/>
+              </Col>
+            </Row>
+          </Container>
+      </div>
+    );
+  }
+  
+}
+
+export default  Consulta1Page;
