2e3cadb7b66d3e722996e70479d3fae5f973812b
DekuCool24 <dayanna.qs@gmail.com>
2020-11-19 03:16:40 -0600

    Creacion Consulta #5
    Se creó la Consulta #5

--- src/Client/Consulta1Page.js
+++ src/Client/Consulta1Page.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
+import { Button } from 'reactstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import { Button } from 'reactstrap';
 import Navbar from 'react-bootstrap/Navbar';
-import { Image } from 'react-bootstrap';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import Nav from 'react-bootstrap/Nav';
 import { Redirect }                              from 'react-router-dom';
 import './WelcomeClient.css';
 import './Consulta1Page.css';
-import logo from '../images/logo.png';
+import Badge from 'react-bootstrap/Badge';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import BootstrapTable                            from 'react-bootstrap-table-next';
+
 
 class Consulta1Page extends Component {
 
@@ -21,17 +25,119 @@ class Consulta1Page extends Component {
         {name: '',
         count: ''}
       ],
-      items: [],
       error: null,
       isLoaded: false,
-      loginFlag: false
+      loginFlag: false,
+      temlist: [],
+      temlistTeach: [],
+      temlistLearn: [],
+
+
+
+      guardarHobbie: [],
+      guardarHobbieJson: [],
+      guardarMedia: [],
+      guardarMediaJson: [],
+      guardarLenguageLearn: [],
+      languageTeach: '',
+      languageLearn: '',
+      jsonFile:{
+        name: '',
+        hobbies: [],
+        media: []
+      },
+
+      resultadosFlag: false,
+      resultadoJson: null,
+      resultado:[],
+
+
+
+
+      hobbielist: [],
+      myhobbielist: [],
+      myhobbielistJson: [],
+      selectHobbie: '',
+      mediaList: [],
+      mymediaList: [],
+      mymedialistJson: [],
+      selectMedia: '',
+      usersList: []
+
     }
+  }
+
+
+  componentDidMount = async (e) => {
+    const userInfo = JSON.parse(localStorage.getItem('user_info'));
+
+    await fetch(`http://localhost:5000/get?continent=${userInfo.region}&collection=user`).catch (err => alert(err))
+    .then(response => response.json())
+    .then(response => this.userAttempt(response))
+    .catch(err => this.errorHandler(err))
+
+
+    await fetch(`http://localhost:5000/get?continent=${userInfo.region}&collection=hobbie`).catch (err => alert(err))
+    .then(response => response.json())
+    .then(response => this.hobbiesAttempt(response))
+    .catch(err => this.errorHandler(err))
+
+    fetch(`http://localhost:5000/get?continent=${userInfo.region}&collection=media`).catch (err => alert(err))
+    .then(response => response.json())
+    .then(response => this.mediaAttempt(response))
+    .catch(err => this.errorHandler(err))
+  }
+
+
+  userAttempt = (res) => {
+    this.setState({
+      usersList: res
+    })
+    this.asignarMisListas();
+  }
+
+
+  asignarMisListas = () => {
+    const userInfo = JSON.parse(localStorage.getItem('user_info'));
+    const name = userInfo.name;
+    for(let i = 0; i < this.state.usersList.length; i++){
+      if(this.state.usersList[i].name === name){
+        this.state.myhobbielist = this.state.usersList[i].hobbies;
+        this.state.mymediaList = this.state.usersList[i].media;
+      }
+    }
+    this.asignarHobbieListJson();
+    this.asignarMediaListJson();
+  }
+
+  asignarMediaListJson = () => {
+    for(let i = 0; i < this.state.mymediaList.length; i++){
+      this.state.mymedialistJson.push({'name': this.state.mymediaList[i]});
+    }
+  }
+
+  asignarHobbieListJson = () => {
+    for(let i = 0; i < this.state.myhobbielist.length; i++){
+      this.state.myhobbielistJson.push({'name': this.state.myhobbielist[i]});
+    }
+  }
+
+  hobbiesAttempt = (res) => {
+    this.setState({
+      hobbielist: res
+    })
+  }
+
+  mediaAttempt = (res) => {
+    this.setState({
+      mediaList: res
+    })
   }
 
   clickPresionado = (event) =>{
     this.setState({
       loginFlag: true
-  })
+    })
   }
 
   renderRedirect = () => {
@@ -40,8 +146,88 @@ class Consulta1Page extends Component {
     }
   }
 
+  clickSelectHobbie = (event) => {
+    this.setState({
+      selectHobbie: event
+    })
+  }
+
+  clickSelectMedia = (event) => {
+    this.setState({
+      selectMedia: event
+    })
+  }
+
+  clickAddMedia = () => {
+    this.state.guardarMedia.push({'name': this.state.selectMedia});
+    this.state.guardarMediaJson.push(this.state.selectMedia);
+  }
+
+  clickAddHobbie = () => {
+    this.state.guardarHobbie.push({'name': this.state.selectHobbie});
+    this.state.guardarHobbieJson.push(this.state.selectHobbie);
+  }
+
+  crearJson = () => {
+    const userInfo = JSON.parse(localStorage.getItem('user_info'));
+    this.state.jsonFile.name = userInfo.name;
+    this.state.jsonFile.hobbies.push(this.state.guardarHobbieJson);
+    this.state.jsonFile.media.push(this.state.guardarMediaJson);
+    console.log('El JSON File');
+    console.log(this.state.jsonFile);
+  }
+
+  clickRealizarBúsqueda = () => {
+    this.busquedaResultados();
+    this.crearJson();
+    this.setState({
+      resultadosFlag: true
+    })
+  }
+
+  busquedaResultados = async () => {
+
+    const userInfo = JSON.parse(localStorage.getItem('user_info'));
+
+    const url = `http://localhost:5000/update?continent=${userInfo.region}`;
+
+    const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(this.state.jsonFile)
+    };
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
+
+  }
+
   render () {
-    return (
+
+    const columnsNewHobbie = [
+      { dataField: 'name', text: 'Hobbie Seleccionado' }
+    ];
+
+    const columnsNewMedio = [
+      { dataField: 'name', text: 'Medio de Contacto Seleccionado' }
+    ];
+
+    const columnsmyHobbie = [
+      { dataField: 'name', text: 'Mi Hobbie Actual' }
+    ];
+
+    const columnsmyMedio = [
+      { dataField: 'name', text: 'Mi Medio de Contacto Actual' }
+    ];
+
+    const columnsRespuesta = [
+      { dataField: 'name', text: 'Nombre' },
+      { dataField: 'age', text: 'Edad' },
+      { dataField: 'gender', text: 'Género' }
+    ];
+    const userInfo = JSON.parse(localStorage.getItem('user_info'));
+    this.state.temlistTeach = userInfo.teach;
+    this.state.temlistLearn = userInfo.learn;
+      return (
       <div className="Consulta1Page">
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 
@@ -64,21 +250,187 @@ class Consulta1Page extends Component {
                 </Navbar>
                 {this.renderRedirect()};
                 <Container>
-              <Row>
-                <Col>
-                <Jumbotron fluid>
-                    <h1>Consulta #5 Modificar mis datos</h1>
-                    <p>
-                    Un usuario puede modificar los siguientes datos:
-                    hobbies, medio por el cual desea ser contactado para practicar el
-                    idioma
-                    </p>
-                </Jumbotron>
-                </Col>
-              </Row>
-            </Container>
-      </div>
-    );
+                  <Row>
+                    <Col>
+                    <Jumbotron fluid>
+                        <h1>Consulta #5 Modificar mis datos</h1>
+                        <p>
+                        Un usuario puede modificar los siguientes datos:
+                        hobbies, medio por el cual desea ser contactado para practicar el
+                        idioma.
+                        </p>
+                    </Jumbotron>
+                    </Col>
+                  </Row>
+                </Container>
+
+
+                <Container>
+                  <Row>
+                        <Col sm="12" md={{ size: 6, offset: 0 }}>
+                        <h3>
+                          <Badge variant="light">Seleccione los nuevos datos para modificar</Badge>
+                        </h3>
+                        </Col>
+                      </Row>
+                  </Container>
+
+                <Container>
+                  <Row>
+
+
+                    <Col>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                        <DropdownButton
+                          as={ButtonGroup}
+                          title={'Escoger Nuevo Hobbie'}
+                          onSelect={this.clickSelectHobbie}
+                        >
+                          {this.state.hobbielist.map((catg) => (
+                          <Dropdown.Item eventKey={catg.name}>{catg.name}</Dropdown.Item>
+                        ))}
+                        </DropdownButton>
+                        <p>{this.state.selectHobbie}</p>
+                        </Col>
+                        <Col md="auto">
+                          <Button onClick={this.clickAddHobbie}
+                          variant="primary" >Añadir</Button>
+                        </Col>
+                      </Row>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                          <BootstrapTable
+                          keyField="name"
+                          data={this.state.guardarHobbie}
+                          columns={columnsNewHobbie}/>
+                        </Col>
+                      </Row>
+                    </Col>
+
+
+                    <Col>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                        <DropdownButton
+                          as={ButtonGroup}
+                          title={'Escoger Nuevo Medio de Contacto'}
+                          onSelect={this.clickSelectMedia}
+                        >
+                          {this.state.mediaList.map((catg) => (
+                          <Dropdown.Item eventKey={catg.name}>{catg.name}</Dropdown.Item>
+                        ))}
+                        </DropdownButton>
+                        <p>{this.state.selectMedia}</p>
+                        </Col>
+                        <Col md="auto">
+                          <Button onClick={this.clickAddMedia}
+                          variant="primary" >Añadir</Button>
+                        </Col>
+                      </Row>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                          <BootstrapTable
+                          keyField="language"
+                          data={this.state.guardarMedia}
+                          columns={columnsNewMedio}/>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+
+
+
+
+                  <br/>
+                            <br/>
+                            <br/>
+
+
+
+
+                  <Row>
+                    <Col sm="12" md={{ size: 6, offset: 0 }}>
+                      <h3>
+                        <Button onClick={this.clickRealizarBúsqueda}
+                        variant="primary" >Realizar Búsqueda</Button>
+                      </h3>
+                    </Col>
+                  </Row>
+
+
+
+
+
+
+
+
+
+                            <br/>
+                            <br/>
+                            <br/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  <Container>
+                  <Row>
+                        <Col sm="12" md={{ size: 6, offset: 0 }}>
+                        <h3>
+                          <Badge variant="light">Mis Hobbies y Medios de Contacto en la Base de Datos</Badge>
+                        </h3>
+                        </Col>
+                      </Row>
+                  </Container>
+
+
+                  <Row>
+
+
+                    <Col>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                          <BootstrapTable
+                          keyField="name"
+                          data={this.state.myhobbielistJson}
+                          columns={columnsmyHobbie}/>
+                        </Col>
+                      </Row>
+                    </Col>
+
+
+                    <Col>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                          <BootstrapTable
+                          keyField="name"
+                          data={this.state.mymedialistJson}
+                          columns={columnsmyMedio}/>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Container>
+                <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div>
+      );
   }
   
 }
