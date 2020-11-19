7077e602004567c54eb91c87629753dc77781520
DekuCool24 <dayanna.qs@gmail.com>
2020-11-19 00:50:14 -0600

    Cambio Orden
    Se cambio el orden del nav de las paginas

--- src/Client/Consulta4Page.js
+++ src/Client/Consulta4Page.js
@@ -207,11 +207,11 @@ class Consulta4Page extends Component {
                 <Navbar.Collapse id="responsive-navbar-nav">
 
                     <Nav className="mr-auto">
-                    <Nav.Link href="./Consulta1Page">Modificar mis datos</Nav.Link>
                     <Nav.Link href="./Consulta2Page">Búsqueda idiomas que Enseño</Nav.Link>
                     <Nav.Link href="./Consulta3Page">Búsqueda idiomas que Enseño y que me enseñen</Nav.Link>
                     <Nav.Link href="./Consulta4Page">Búsqueda idiomas que Enseño y que me enseñen por País</Nav.Link>
                     <Nav.Link href="./Consulta5Page">Búsqueda idiomas que Enseño y que me enseñen por País y rango Edad</Nav.Link>
+                    <Nav.Link href="./Consulta1Page">Modificar mis datos</Nav.Link>
                     <Nav.Link onClick={this.clickPresionado}>LogOut</Nav.Link>
                     </Nav>
 
@@ -348,6 +348,8 @@ class Consulta4Page extends Component {
                           </Col>
                         </Row>
                       </Col>
+
+\
                   </Row>
 
 
@@ -403,11 +405,11 @@ class Consulta4Page extends Component {
                   <Navbar.Collapse id="responsive-navbar-nav">
   
                       <Nav className="mr-auto">
-                      <Nav.Link href="./Consulta1Page">Modificar mis datos</Nav.Link>
                       <Nav.Link href="./Consulta2Page">Búsqueda idiomas que Enseño</Nav.Link>
                       <Nav.Link href="./Consulta3Page">Búsqueda idiomas que Enseño y que me enseñen</Nav.Link>
                       <Nav.Link href="./Consulta4Page">Búsqueda idiomas que Enseño y que me enseñen por País</Nav.Link>
                       <Nav.Link href="./Consulta5Page">Búsqueda idiomas que Enseño y que me enseñen por País y rango Edad</Nav.Link>
+                      <Nav.Link href="./Consulta1Page">Modificar mis datos</Nav.Link>
                       <Nav.Link onClick={this.clickPresionado}>LogOut</Nav.Link>
                       </Nav>
   
@@ -546,6 +548,8 @@ class Consulta4Page extends Component {
                           </Col>
                         </Row>
                       </Col>
+
+
 
 
                     </Row>
