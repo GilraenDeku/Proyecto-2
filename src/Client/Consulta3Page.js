b0a091d13b1a825ae1011d6a5a90166185760af7
DekuCool24 <dayanna.qs@gmail.com>
2020-11-18 22:39:02 -0600

    Creacion Consulta 3
    Se creo la pagina de la consutla 3

--- src/Client/Consulta3Page.js
+++ src/Client/Consulta3Page.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
+import { Button } from 'reactstrap';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
-import { Image } from 'react-bootstrap';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import Nav from 'react-bootstrap/Nav';
 import { Redirect }                              from 'react-router-dom';
 import './WelcomeClient.css';
-import logo from '../images/logo.png';
+import './Consulta3Page.css';
+import Badge from 'react-bootstrap/Badge';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import BootstrapTable                            from 'react-bootstrap-table-next';
 
 class Consulta3Page extends Component {
 
@@ -21,14 +27,34 @@ class Consulta3Page extends Component {
       items: [],
       error: null,
       isLoaded: false,
-      loginFlag: false
+      loginFlag: false,
+      tempListLanguage: [],
+      temlist: [],
+      temlistTeach: [],
+      temlistLearn: [],
+
+
+
+      guardarLenguageTeach: [],
+      guardarLenguageLearn: [],
+      languageTeach: '',
+      languageLearn: '',
+      jsonFile:{
+        learn: [],
+        teach: []
+      },
+
+      resultadosFlag: false,
+      resultadoJson: null,
+      resultado:[]
+
     }
   }
 
   clickPresionado = (event) =>{
     this.setState({
       loginFlag: true
-  })
+    })
   }
 
   renderRedirect = () => {
@@ -37,8 +63,93 @@ class Consulta3Page extends Component {
     }
   }
 
+  clickSelectLanguageTeach = (event) => {
+    this.setState({
+      languageTeach: event
+    })
+  }
+
+  clickSelectLanguageLearn = (event) => {
+    this.setState({
+      languageLearn: event
+    })
+  }
+
+  clickAddLanguagelearn = () => {
+    if(this.state.guardarLenguageLearn.length === this.state.temlistLearn.length){
+
+    }else{
+      this.state.guardarLenguageLearn.push({'language': this.state.languageLearn});
+      this.state.jsonFile.learn.push(this.state.languageLearn);
+    }
+  }
+
+  clickAddLanguageteach = () => {
+    if(this.state.guardarLenguageTeach.length === this.state.temlistTeach.length){
+
+    }else{
+      this.state.guardarLenguageTeach.push({'language': this.state.languageTeach});
+      this.state.jsonFile.teach.push(this.state.languageTeach);
+    }
+  }
+
+  clickRealizarBúsqueda = () => {
+    this.busquedaResultados();
+    this.setState({
+      resultadosFlag: true
+    })
+  }
+
+  busquedaResultados = async () => {
+
+    const userInfo = JSON.parse(localStorage.getItem('user_info'));
+
+    const url = `http://localhost:5000/people_learn_teach?continent=${userInfo.region}`;
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(this.state.jsonFile)
+    };
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
+    this.state.resultadoJson = data;
+    this.creacionListaTabla();
+
+  }
+
+  creacionListaTabla = () => {
+    if(this.state.resultado.length === this.state.resultadoJson.length){
+
+    }
+    else{
+
+      for(let i = 0; i < this.state.resultadoJson.length; i++){
+        this.state.resultado.push({
+          'name': this.state.resultadoJson[i].name,
+          'age': this.state.resultadoJson[i].age,
+          'gender': this.state.resultadoJson[i].gender
+        })
+      }
+
+    }
+  }
+
   render () {
-    return (
+    const columnslanguage = [
+      { dataField: 'language', text: 'Lenguaje Seleccionado' }
+    ];
+
+    const columnsRespuesta = [
+      { dataField: 'name', text: 'Nombre' },
+      { dataField: 'age', text: 'Edad' },
+      { dataField: 'gender', text: 'Género' }
+    ];
+    const userInfo = JSON.parse(localStorage.getItem('user_info'));
+    this.state.temlistTeach = userInfo.teach;
+    this.state.temlistLearn = userInfo.learn;
+    if(this.state.resultadosFlag){
+      return (
       <div className="Consulta3Page">
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 
@@ -60,40 +171,263 @@ class Consulta3Page extends Component {
                 </Navbar.Collapse>
                 </Navbar>
                 {this.renderRedirect()};
-          <Container>
-            <Row>
-              <Col>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <Image src={logo} fluid />
-              <br/>
-              <br/>
-              <br/>
-                  <h1>Consulta #3</h1>
-                  <p>
-                  Esta es la página de la Consulta #3
-                  </p>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              </Col>
-            </Row>
-          </Container>
-      </div>
-    );
+                <Container>
+                  <Row>
+                    <Col>
+                    <Jumbotron fluid>
+                        <h1>Consulta #2 Búsqueda idiomas que Enseño y que me enseñen</h1>
+                        <p>
+                        Buscar a otros interesados en practicar uno o más de los idiomas que la
+                        persona P puede enseñar y que estos puedan enseñar uno o más de los
+                        idiomas que la persona P desea practicar
+                        </p>
+                    </Jumbotron>
+                    </Col>
+                  </Row>
+                </Container>
+
+                <Container>
+                  <Row>
+
+
+                    <Col>
+                      <Row>
+                        <Col sm="12" md={{ size: 6, offset: 0 }}>
+                        <h3>
+                          <Badge variant="light">Seleccione el o los idiomas que desea buscar</Badge>
+                        </h3>
+                        </Col>
+                      </Row>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                        <DropdownButton
+                          as={ButtonGroup}
+                          title={'Escoja Lenguaje Enseñar'}
+                          onSelect={this.clickSelectLanguageTeach}
+                        >
+                          {this.state.temlistTeach.map((catg) => (
+                          <Dropdown.Item eventKey={catg.language}>{catg.language}</Dropdown.Item>
+                        ))}
+                        </DropdownButton>
+                        <p>{this.state.languageTeach}</p>
+                        </Col>
+                        <Col md="auto">
+                          <Button onClick={this.clickAddLanguageteach}
+                          variant="primary" >Añadir</Button>
+                        </Col>
+                      </Row>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                          <BootstrapTable
+                          keyField="language"
+                          data={this.state.guardarLenguageTeach}
+                          columns={columnslanguage}/>
+                        </Col>
+                      </Row>
+                    </Col>
+
+
+                    <Col>
+                      <Row>
+                        <Col sm="12" md={{ size: 6, offset: 0 }}>
+                        <h3>
+                          <Badge variant="light">Seleccione el o los idiomas que desea buscar</Badge>
+                        </h3>
+                        </Col>
+                      </Row>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                        <DropdownButton
+                          as={ButtonGroup}
+                          title={'Escoja Lenguaje Practicar'}
+                          onSelect={this.clickSelectLanguageLearn}
+                        >
+                          {this.state.temlistLearn.map((catg) => (
+                          <Dropdown.Item eventKey={catg.language}>{catg.language}</Dropdown.Item>
+                        ))}
+                        </DropdownButton>
+                        <p>{this.state.languageLearn}</p>
+                        </Col>
+                        <Col md="auto">
+                          <Button onClick={this.clickAddLanguagelearn}
+                          variant="primary" >Añadir</Button>
+                        </Col>
+                      </Row>
+                      <Row className="justify-content-md-center">
+                        <Col md="auto">
+                          <BootstrapTable
+                          keyField="language"
+                          data={this.state.guardarLenguageLearn}
+                          columns={columnslanguage}/>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col sm="12" md={{ size: 6, offset: 0 }}>
+                      <h3>
+                        <Button onClick={this.clickRealizarBúsqueda}
+                        variant="primary" >Realizar Búsqueda</Button>
+                      </h3>
+                    </Col>
+                  </Row>
+
+                  <Row>
+                    <Col sm="12" md={{ size: 6, offset: 0 }}>
+                    <BootstrapTable
+                          keyField="name"
+                          data={this.state.resultado}
+                          columns={columnsRespuesta}/>
+                    </Col>
+                  </Row>
+                </Container>
+                <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div>
+      );
+    }
+    else{
+      return (
+        <div className="Consulta3Page">
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                  
+                  <Navbar.Brand href="./WelcomeClient">Bienvenido</Navbar.Brand>
+                  
+                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                  
+                  <Navbar.Collapse id="responsive-navbar-nav">
+  
+                      <Nav className="mr-auto">
+                      <Nav.Link href="./Consulta1Page">Modificar mis datos</Nav.Link>
+                      <Nav.Link href="./Consulta2Page">Búsqueda idiomas que Enseño</Nav.Link>
+                      <Nav.Link href="./Consulta3Page">Búsqueda idiomas que Enseño y que me enseñen</Nav.Link>
+                      <Nav.Link href="./Consulta4Page">Búsqueda idiomas que Enseño y que me enseñen por País</Nav.Link>
+                      <Nav.Link href="./Consulta5Page">Búsqueda idiomas que Enseño y que me enseñen por País y rango Edad</Nav.Link>
+                      <Nav.Link onClick={this.clickPresionado}>LogOut</Nav.Link>
+                      </Nav>
+  
+                  </Navbar.Collapse>
+                  </Navbar>
+                  {this.renderRedirect()};
+                  <Container>
+                    <Row>
+                      <Col>
+                      <Jumbotron fluid>
+                          <h1>Consulta #2 Búsqueda idiomas que Enseño y que me enseñen</h1>
+                          <p>
+                          Buscar a otros interesados en practicar uno o más de los idiomas que la
+                          persona P puede enseñar y que estos puedan enseñar uno o más de los
+                          idiomas que la persona P desea practicar
+                          </p>
+                      </Jumbotron>
+                      </Col>
+                    </Row>
+                  </Container>
+  
+                  <Container>
+                    <Row>
+  
+  
+                      <Col>
+                        <Row>
+                          <Col sm="12" md={{ size: 6, offset: 0 }}>
+                          <h3>
+                            <Badge variant="light">Seleccione el o los idiomas que desea buscar</Badge>
+                          </h3>
+                          </Col>
+                        </Row>
+                        <Row className="justify-content-md-center">
+                          <Col md="auto">
+                          <DropdownButton
+                            as={ButtonGroup}
+                            title={'Escoja Lenguaje Enseñar'}
+                            onSelect={this.clickSelectLanguageTeach}
+                          >
+                            {this.state.temlistTeach.map((catg) => (
+                            <Dropdown.Item eventKey={catg.language}>{catg.language}</Dropdown.Item>
+                          ))}
+                          </DropdownButton>
+                          <p>{this.state.languageTeach}</p>
+                          </Col>
+                          <Col md="auto">
+                            <Button onClick={this.clickAddLanguageteach}
+                            variant="primary" >Añadir</Button>
+                          </Col>
+                        </Row>
+                        <Row className="justify-content-md-center">
+                          <Col md="auto">
+                            <BootstrapTable
+                            keyField="language"
+                            data={this.state.guardarLenguageTeach}
+                            columns={columnslanguage}/>
+                          </Col>
+                        </Row>
+                      </Col>
+  
+  
+                      <Col>
+                        <Row>
+                          <Col sm="12" md={{ size: 6, offset: 0 }}>
+                          <h3>
+                            <Badge variant="light">Seleccione el o los idiomas que desea buscar</Badge>
+                          </h3>
+                          </Col>
+                        </Row>
+                        <Row className="justify-content-md-center">
+                          <Col md="auto">
+                          <DropdownButton
+                            as={ButtonGroup}
+                            title={'Escoja Lenguaje Practicar'}
+                            onSelect={this.clickSelectLanguageLearn}
+                          >
+                            {this.state.temlistLearn.map((catg) => (
+                            <Dropdown.Item eventKey={catg.language}>{catg.language}</Dropdown.Item>
+                          ))}
+                          </DropdownButton>
+                          <p>{this.state.languageLearn}</p>
+                          </Col>
+                          <Col md="auto">
+                            <Button onClick={this.clickAddLanguagelearn}
+                            variant="primary" >Añadir</Button>
+                          </Col>
+                        </Row>
+                        <Row className="justify-content-md-center">
+                          <Col md="auto">
+                            <BootstrapTable
+                            keyField="language"
+                            data={this.state.guardarLenguageLearn}
+                            columns={columnslanguage}/>
+                          </Col>
+                        </Row>
+                      </Col>
+                    </Row>
+                    <Row>
+                    <Col sm="12" md={{ size: 6, offset: 0 }}>
+                      <h3>
+                        <Button onClick={this.clickRealizarBúsqueda}
+                        variant="primary" >Realizar Búsqueda</Button>
+                      </h3>
+                    </Col>
+                  </Row>
+                  </Container>
+                  <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div>
+      );
+    }
   }
   
 }
