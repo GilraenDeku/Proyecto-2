e50261d19ce72ea358f365ac0116e66ee602c842
DekuCool24 <dayanna.qs@gmail.com>
2020-11-23 10:50:48 -0600

    Update WelcomeClient.js

--- src/Client/WelcomeClient.js
+++ src/Client/WelcomeClient.js
@@ -5,18 +5,20 @@ import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import { Image } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
-import { Redirect }                              from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import './WelcomeClient.css';
 import logo from '../images/logo.png';
 
 class WelcomeClient extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state ={ 
-      generalLanguage:[
-        {name: '',
-        count: ''}
+    this.state = {
+      generalLanguage: [
+        {
+          name: '',
+          count: ''
+        }
       ],
       items: [],
       error: null,
@@ -25,10 +27,10 @@ class WelcomeClient extends Component {
     }
   }
 
-  clickPresionado = (event) =>{
+  clickPresionado = (event) => {
     this.setState({
       loginFlag: true
-  })
+    })
   }
 
   renderRedirect = () => {
@@ -37,8 +39,8 @@ class WelcomeClient extends Component {
     }
   }
 
-  render () {
-    
+  render() {
+
     const userInfo = JSON.parse(localStorage.getItem('user_info'))
 
     console.log('Region que el usuario escogió');
@@ -49,61 +51,61 @@ class WelcomeClient extends Component {
     return (
       <div className="WelcomeClient">
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                
-                <Navbar.Brand href="./WelcomeClient">Bienvenido</Navbar.Brand>
-                
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                
-                <Navbar.Collapse id="responsive-navbar-nav">
 
-                    <Nav className="mr-auto">
-                    <Nav.Link href="./Consulta2Page">Búsqueda idiomas que Enseño</Nav.Link>
-                    <Nav.Link href="./Consulta3Page">Búsqueda idiomas que Enseño y que me enseñen</Nav.Link>
-                    <Nav.Link href="./Consulta4Page">Búsqueda idiomas que Enseño y que me enseñen por País</Nav.Link>
-                    <Nav.Link href="./Consulta5Page">Búsqueda idiomas que Enseño y que me enseñen por País y rango Edad</Nav.Link>
-                    <Nav.Link href="./Consulta1Page">Modificar mis datos</Nav.Link>
-                    <Nav.Link onClick={this.clickPresionado}>LogOut</Nav.Link>
-                    </Nav>
+          <Navbar.Brand href="./WelcomeClient">Bienvenido</Navbar.Brand>
 
-                </Navbar.Collapse>
-                </Navbar>
-                {this.renderRedirect()};
-          <Container>
-            <Row>
-              <Col>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+          <Navbar.Collapse id="responsive-navbar-nav">
+
+            <Nav className="mr-auto">
+              <Nav.Link href="./Consulta2Page">Búsqueda idiomas que Enseño</Nav.Link>
+              <Nav.Link href="./Consulta3Page">Búsqueda idiomas que Enseño y que me enseñen</Nav.Link>
+              <Nav.Link href="./Consulta4Page">Búsqueda idiomas que Enseño y que me enseñen por País</Nav.Link>
+              <Nav.Link href="./Consulta5Page">Búsqueda idiomas que Enseño y que me enseñen por País y rango Edad</Nav.Link>
+              <Nav.Link href="./Consulta1Page">Modificar mis datos</Nav.Link>
+              <Nav.Link onClick={this.clickPresionado}>LogOut</Nav.Link>
+            </Nav>
+
+          </Navbar.Collapse>
+        </Navbar>
+        {this.renderRedirect()};
+        <Container>
+          <Row>
+            <Col>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
               <Image src={logo} fluid />
-              <br/>
-              <br/>
-              <br/>
-                  <h1>Bienvenido {userInfo.name}</h1>
-                  <p>
-                  Esperamos que disfrute las búsquedas
+              <br />
+              <br />
+              <br />
+              <h1>Bienvenido {userInfo.name}</h1>
+              <p>
+                Esperamos que disfrute las búsquedas
                   </p>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              <br/>
-              </Col>
-            </Row>
-          </Container>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
-  
+
 }
 
-export default  WelcomeClient;
+export default WelcomeClient;
